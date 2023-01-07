ee87a055f179858216101363008d46b3312a24bc
fundakartal <kartalfunda01@gmail.com>
2023-01-07 15:15:37 +0300

    Configure ESLint

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,7 @@
-import './globals.css'
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>{children}</body>
+  </html>
+);
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
-}
+export default RootLayout;
